4c112e1e943bc191a5dead4dd81dae57ab0b4225
Burak <92034574+brkylmz111@users.noreply.github.com>
2022-03-26 23:19:12 +0300

    Update PhoneListModel.cs

--- contacts-main/Model/PhoneListModel.cs
+++ contacts-main/Model/PhoneListModel.cs
@@ -2,7 +2,7 @@
 
 namespace TelefonRehberi
 {
-    //Bir veri tabanı kullanmadığım için her yerden ulaşılabilmesi gereken bir Rehbere ihtiyacım vardı. Bu amaçla static bir obje tanımlayarak referansı ile her yerde kullanabildim.
+    
     public static class PhoneListModel
     {
         public static List<NumberModel> PhoneNumberList = new List<NumberModel>();
